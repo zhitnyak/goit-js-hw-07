491c42abf109d4ba168e7527171e0623baa04e94
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-06-05 14:31:29 +0300

    Task_6

--- js/task-06.js
+++ js/task-06.js
@@ -1,21 +1,27 @@
 // Напиши скрипт, который бы при потере фокуса на инпуте, проверял его содержимое
 // на правильное количество символов.
 
-// <input
-//   type="text"
-//   id="validation-input"
-//   data-length="6"
-//   placeholder="Введи 6 символов"/>
 // Сколько символов должно быть в инпуте, указывается в его атрибуте data-length.
 // Если введено подходящее количество, то border инпута становится зеленым, если
 // неправильное - красным.
 // Для добавления стилей, используй CSS-классы valid и invalid.
 
-// #validation-input {
-//   border: 3px solid #bdbdbd}
-// #validation-input.valid {
-//   border-color: #4caf50}
-// #validation-input.invalid {
-//   border-color: #f44336 }
-
 const inputText = document.querySelector("#validation-input");
+
+const carrentLength = Number(inputText.getAttribute("data-length"));
+// console.log(typeof carrentLength);
+
+const onInputBlur = () => {
+  //   if (inputText.value.length === 0) {
+  //     inputText.classList.remove("invalid");
+  //     inputText.classList.remove("valid");
+  //   }
+  if (inputText.value.length === carrentLength) {
+    inputText.classList.add("valid");
+    inputText.classList.remove("invalid");
+  } else {
+    inputText.classList.add("invalid");
+    inputText.classList.remove("valid");
+  }
+};
+inputText.addEventListener("blur", onInputBlur);
